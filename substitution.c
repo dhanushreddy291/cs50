d7809f6d3847b9f4c7c9b4cea0aaa2e8d28bff0a
dhanushreddy291 <dhanushreddy291@users.noreply.github.com>
2020-12-25 16:13:13 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -63,8 +63,8 @@ int main(int argc, string argv[])
 
     }
 
-    string plaintext = get_string("plaintext : ") ;
-    printf("ciphertext : ") ;
+    string plaintext = get_string("plaintext: ") ;
+    printf("ciphertext: ") ;
 
     for (int i = 0 ; i < strlen(plaintext) ; i++)
     {
