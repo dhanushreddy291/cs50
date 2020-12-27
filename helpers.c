efd5d69632bc4cb4e9eedd4afbc0141bf096d53b
dhanushreddy291 <dhanushreddy291@users.noreply.github.com>
2020-12-27 16:08:10 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -52,6 +52,43 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE tempimage[height][width];
+
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            double sum[3] = {0};
+            int counter = 0;
+            
+            for (int k = i - 1; k > 0 && k < height && k < i + 2; k++)
+            {
+                for (int l = j - 1; l > 0 && l < width && l < j + 2; l++)
+                {
+                    sum[0] += image[k][l].rgbtBlue;
+                    sum[1] += image[k][l].rgbtGreen;
+                    sum[2] += image[k][l].rgbtRed;
+                    counter++;
+                }
+            }
+            
+            tempimage[i][j].rgbtBlue = round(sum[0] / ((1.00) * counter));
+            tempimage[i][j].rgbtGreen = round(sum[1] /((1.00) * counter));
+            tempimage[i][j].rgbtRed = round(sum[2] / ((1.00) * counter));
+        }
+    }
+
+    for (int i = 0; i< height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtBlue = tempimage[i][j].rgbtBlue;
+            image[i][j].rgbtGreen = tempimage[i][j].rgbtGreen;
+            image[i][j].rgbtRed = tempimage[i][j].rgbtRed;
+        }
+
+    }
+
     return;
 }
 
