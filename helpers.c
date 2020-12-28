e6bca60362adf0780f464dac6dad8513ae6a6c0f
dhanushreddy291 <dhanushreddy291@users.noreply.github.com>
2020-12-28 07:30:41 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -8,7 +8,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int averagergb = round(( image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed ) / 3.00 );
+            int averagergb = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.00);
 
             image[i][j].rgbtBlue = averagergb;
             image[i][j].rgbtGreen = averagergb;
@@ -28,7 +28,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width/2; j++)
+        for (int j = 0; j < width / 2; j++)
         {
             temp.rgbtBlue = image[i][j].rgbtBlue;
             temp.rgbtGreen = image[i][j].rgbtGreen;
@@ -54,9 +54,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE tempimage[height][width];
 
-    for(int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
             double sum[3] = {0, 0, 0}, counter = 0;
 
@@ -71,12 +71,12 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 }
             }
 
-            sum[0] = round(sum[0] / ((1.00) * counter));
-            sum[1] = round(sum[1] / ((1.00) * counter));
-            sum[2] = round(sum[2] / ((1.00) * counter));
+            sum[0] = sum[0] / ((1.00) * counter);
+            sum[1] = sum[1] / ((1.00) * counter);
+            sum[2] = sum[2] / ((1.00) * counter);
 
 
-            if(sum[0] > 255)
+            if (sum[0] > 255)
             {
                 tempimage[i][j].rgbtBlue = 255;
             }
@@ -85,7 +85,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 tempimage[i][j].rgbtBlue = (int)sum[0];
             }
 
-            if(sum[1] > 255)
+            if (sum[1] > 255)
             {
                 tempimage[i][j].rgbtGreen = 255;
             }
@@ -94,7 +94,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 tempimage[i][j].rgbtGreen = (int)sum[1];
             }
 
-            if(sum[2] > 255)
+            if (sum[2] > 255)
             {
                 tempimage[i][j].rgbtRed = 255;
             }
@@ -107,7 +107,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 
     for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
             image[i][j].rgbtBlue = tempimage[i][j].rgbtBlue;
             image[i][j].rgbtGreen = tempimage[i][j].rgbtGreen;
@@ -169,20 +169,30 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
         {
             double sum[9];
 
-            sum[0] = (-tmpimage[i - 1][j - 1].rgbtBlue) + (tmpimage[i - 1][j + 1].rgbtBlue) - (2 * tmpimage[i][j - 1].rgbtBlue) + (2 * tmpimage[i][j + 1].rgbtBlue) - (tmpimage[i + 1][j - 1].rgbtBlue) + (tmpimage[i + 1][j + 1].rgbtBlue);
-            sum[1] = (-tmpimage[i - 1][j - 1].rgbtGreen) + (tmpimage[i - 1][j + 1].rgbtGreen) - (2 * tmpimage[i][j - 1].rgbtGreen) + (2 * tmpimage[i][j + 1].rgbtGreen) - (tmpimage[i + 1][j - 1].rgbtGreen) + (tmpimage[i + 1][j + 1].rgbtGreen);
-            sum[2] = (-tmpimage[i - 1][j - 1].rgbtRed) + (tmpimage[i - 1][j + 1].rgbtRed) - (2 * tmpimage[i][j - 1].rgbtRed) + (2 * tmpimage[i][j + 1].rgbtRed) - (tmpimage[i + 1][j - 1].rgbtRed) + (tmpimage[i + 1][j + 1].rgbtRed);
+            sum[0] = (-tmpimage[i - 1][j - 1].rgbtBlue) + (tmpimage[i - 1][j + 1].rgbtBlue) - (2 * tmpimage[i][j - 1].rgbtBlue) + 
+                     (2 * tmpimage[i][j + 1].rgbtBlue) - (tmpimage[i + 1][j - 1].rgbtBlue) + (tmpimage[i + 1][j + 1].rgbtBlue);
+                     
+            sum[1] = (-tmpimage[i - 1][j - 1].rgbtGreen) + (tmpimage[i - 1][j + 1].rgbtGreen) - (2 * tmpimage[i][j - 1].rgbtGreen) + 
+                     (2 * tmpimage[i][j + 1].rgbtGreen) - (tmpimage[i + 1][j - 1].rgbtGreen) + (tmpimage[i + 1][j + 1].rgbtGreen);
+                     
+            sum[2] = (-tmpimage[i - 1][j - 1].rgbtRed) + (tmpimage[i - 1][j + 1].rgbtRed) - (2 * tmpimage[i][j - 1].rgbtRed) + 
+                     (2 * tmpimage[i][j + 1].rgbtRed) - (tmpimage[i + 1][j - 1].rgbtRed) + (tmpimage[i + 1][j + 1].rgbtRed);
 
-            sum[3] = (-tmpimage[i - 1][j - 1].rgbtBlue) - (2 * tmpimage[i - 1][j].rgbtBlue) - (tmpimage[i - 1][j + 1].rgbtBlue) + (tmpimage[i + 1][j - 1].rgbtBlue) + (2 * tmpimage[i + 1][j].rgbtBlue) + (tmpimage[i + 1][j + 1].rgbtBlue);
-            sum[4] = (-tmpimage[i - 1][j - 1].rgbtGreen) - (2 * tmpimage[i - 1][j].rgbtGreen) - (tmpimage[i - 1][j + 1].rgbtGreen) + (tmpimage[i + 1][j - 1].rgbtGreen) + (2 * tmpimage[i + 1][j].rgbtGreen) + (tmpimage[i + 1][j + 1].rgbtGreen);
-            sum[5] = (-tmpimage[i - 1][j - 1].rgbtRed) - (2 * tmpimage[i - 1][j].rgbtRed) - (tmpimage[i - 1][j + 1].rgbtRed) + (tmpimage[i + 1][j - 1].rgbtRed) + (2 * tmpimage[i + 1][j].rgbtRed) + (tmpimage[i + 1][j + 1].rgbtRed);
+            sum[3] = (-tmpimage[i - 1][j - 1].rgbtBlue) - (2 * tmpimage[i - 1][j].rgbtBlue) - (tmpimage[i - 1][j + 1].rgbtBlue) + 
+                     (tmpimage[i + 1][j - 1].rgbtBlue) + (2 * tmpimage[i + 1][j].rgbtBlue) + (tmpimage[i + 1][j + 1].rgbtBlue);
+                     
+            sum[4] = (-tmpimage[i - 1][j - 1].rgbtGreen) - (2 * tmpimage[i - 1][j].rgbtGreen) - (tmpimage[i - 1][j + 1].rgbtGreen) + 
+                     (tmpimage[i + 1][j - 1].rgbtGreen) + (2 * tmpimage[i + 1][j].rgbtGreen) + (tmpimage[i + 1][j + 1].rgbtGreen);
+                     
+            sum[5] = (-tmpimage[i - 1][j - 1].rgbtRed) - (2 * tmpimage[i - 1][j].rgbtRed) - (tmpimage[i - 1][j + 1].rgbtRed) + 
+                     (tmpimage[i + 1][j - 1].rgbtRed) + (2 * tmpimage[i + 1][j].rgbtRed) + (tmpimage[i + 1][j + 1].rgbtRed);
 
 
-            sum[6] = round( pow( pow(sum[0], 2) + pow(sum[3], 2) , 0.5));
-            sum[7] = round( pow( pow(sum[1], 2) + pow(sum[4], 2) , 0.5));
-            sum[8] = round( pow( pow(sum[2], 2) + pow(sum[5], 2) , 0.5));
+            sum[6] = round(pow(pow(sum[0], 2) + pow(sum[3], 2), 0.5));
+            sum[7] = round(pow(pow(sum[1], 2) + pow(sum[4], 2), 0.5));
+            sum[8] = round(pow(pow(sum[2], 2) + pow(sum[5], 2), 0.5));
 
-            if(sum[6] > 255)
+            if (sum[6] > 255)
             {
                 image[i - 1][j - 1].rgbtBlue = 255;
 
@@ -193,7 +203,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             }
 
 
-            if(sum[7] > 255)
+            if (sum[7] > 255)
             {
                 image[i - 1][j - 1].rgbtGreen = 255;
 
@@ -204,7 +214,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             }
 
 
-            if(sum[8] > 255)
+            if (sum[8] > 255)
             {
                 image[i - 1][j - 1].rgbtRed = 255;
 
