57f846cf688cc1ce68aed1aaaba611d8c98c6d89
dhanushreddy291 <dhanushreddy291@users.noreply.github.com>
2020-12-29 16:39:37 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,13 +1,19 @@
 from cs50 import get_int
 
+#Loop doesnot exit until the user inputs a number between 1 to 8 inclusive
 while True:
     height = get_int("Height : ")
     if 0 < height < 9:
         break
 
+#A loop to print require Pyramid
 for i in range(height):
+    
+    #Prints white spaces
     for j in range(height - i - 1):
-        print(" ", end = "")
+        print(" ", end="")
+    
+    #Prints #
     for k in range(i + 1):
-        print("#", end = "")
+        print("#", end="")
     print()    
